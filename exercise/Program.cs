2293dc62b21c3241e89932831c7c540c3ff969b8
khammad5 <khammadakh@code.edu.az>
2023-10-25 13:15:04 +0400

    Add project files.

---
+++ exercise/Program.cs
@@ -0,0 +1,161 @@
+﻿//int time = 20;
+//string result = (time < 18) ? "Good day." : "Good evening.";
+//Console.WriteLine(result);
+
+//int i = 1;
+//while (i < 6)
+//{
+//    Console.WriteLine(i);
+//    i++;
+//}
+
+//int i = 1;
+//do
+//{
+//    Console.WriteLine(i);
+//    i++;
+
+//}
+//while (i<6);
+
+
+//for (int i = 0; i < 5; i++) 
+//{
+
+//    Console.WriteLine("yes");
+//}
+
+//for (int i = 0; i < 10; i++)
+//{
+//    if (i == 4) 
+//    {
+//      continue;
+//    }
+//    Console.WriteLine(i);
+//}
+
+
+
+//string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+//Console.WriteLine(cars[0]);
+
+//int num = 8;
+//if (num == 0)
+//{
+//    Console.WriteLine("basqa eded daxil edin");
+//}
+//else
+//{
+//    if (num % 2 == 0)
+//    {
+//        Console.WriteLine("bolunur");
+//    }
+//    else
+//    {
+//        Console.WriteLine("bolunur");
+//    }
+
+//}
+
+//Console.WriteLine("enter number: ");
+
+//int num = int.Parse(Console.ReadLine());
+//if (num == 0)
+//{
+//    Console.WriteLine("basqa ede daxil edin");
+//}
+//else
+//{
+//    if (num % 2 == 0)
+//    {
+//        Console.WriteLine("bolunur");
+//    }
+//    else
+//    {
+//        Console.WriteLine("bolunmur...");
+//    }
+//}
+
+//Console.WriteLine("eded daxil edin");
+//int num = int.Parse(Console.ReadLine());
+//if (num == 0)
+//{
+//    Console.WriteLine("basqa ede daxil edin");
+//}
+//else
+//{
+//    if (num % 2 == 0)
+//    {
+//        Console.WriteLine("bolunur");
+//    }
+//    else
+//    {
+//        Console.WriteLine("bolunmur");
+//    }
+//}
+
+//int i= 15;
+
+//for (int num = 1; num <=i; num++)
+//{
+//    Console.WriteLine(num);
+//}
+
+
+//int n = 4;
+//int m = 7;
+//int result = 0;
+//if (n > m)
+//{
+//    Console.WriteLine("n>m ola bilmez");
+//}
+//else
+//{
+//    for (int i = n; i <= m; i++)
+//    {
+//        result += i;
+//    }
+//    Console.WriteLine(result);
+//}
+
+
+//int a = 3;
+//int b = 8;
+//int result = 0;
+
+//for (int i = a; i<=b; i++) 
+//{
+//    if (i % 2 == 1) 
+//    {
+//        result += i;
+//    }
+//}
+//Console.WriteLine(result);
+
+
+//int a = 23;
+//int b = 45;
+//int result = 0;
+
+//for (int i=a; i<=b; i++) 
+//{
+//    if (i % 2 == 0) 
+//    {
+//        result += i;
+//    }
+
+//}
+//Console.WriteLine(result);
+
+//int t = 23;
+//int y = 45;
+//int result = 0;
+
+//for (int i = t; i<=y; i++) 
+//{
+//    if (i % 2 == 1) 
+//    {
+//        result += i;
+//    }
+//} Console.WriteLine(result);
+
